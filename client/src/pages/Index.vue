e2251ac871501dde29d826dffc1512f9b9c99b3e
0xEnthusiast <sergexa.kulikov2003@gmail.com>
2024-06-26 00:05:03 +0500

    build(client): install shadcn-vue with deps

---
+++ client/src/pages/Index.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { Button } from '@/components/ui/button'
+import {ref} from "vue";
+
+const count = ref(0);
+
+</script>
+
+<template>
+  Hello World <br>
+  Current count is: {{ count }}
+  <div>
+    <Button @click="count++">Click me</Button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
