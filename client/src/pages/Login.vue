32b8bab2b7a365fe84e069c555322f44e4424109
0xEnthusiast <sergexa.kulikov2003@gmail.com>
2024-06-26 02:46:07 +0500

    idk

--- client/src/pages/Login.vue
+++ client/src/pages/Login.vue
@@ -1,73 +1,54 @@
 <script setup lang="ts">
-// import {Layout} from "@/components/layouts";
-
 import { Button } from '@/components/ui/button'
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 
 import {useColorMode} from "@vueuse/core";
-
 const mode = useColorMode()
 
 </script>
 
 <template>
-  <div class="w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px]">
-    <div class="flex items-center justify-center py-12">
-      <div class="mx-auto grid w-[350px] gap-6">
-        <div class="grid gap-2 text-center">
-          <h1 class="text-3xl font-bold">
-            Login
-          </h1>
-          <p class="text-balance text-muted-foreground">
-            Enter your email below to login to your account
-          </p>
+  <Card class="m-auto max-w-sm">
+    <CardHeader>
+      <CardTitle class="text-2xl">
+        Login
+      </CardTitle>
+      <CardDescription>
+        Enter your email below to login to your account
+      </CardDescription>
+    </CardHeader>
+    <CardContent>
+      <div class="grid gap-4">
+        <div class="grid gap-2">
+          <Label for="email">Email</Label>
+          <Input
+              id="email"
+              type="email"
+              placeholder="m@example.com"
+              required
+          />
         </div>
-        <div class="grid gap-4">
-          <div class="grid gap-2">
-            <Label for="email">Email</Label>
-            <Input
-                id="email"
-                type="email"
-                placeholder="m@example.com"
-                required
-            />
+        <div class="grid gap-2">
+          <div class="flex items-center">
+            <Label for="password">Password</Label>
+            <a href="#" class="ml-auto inline-block text-sm underline">
+              Forgot your password?
+            </a>
           </div>
-          <div class="grid gap-2">
-            <div class="flex items-center">
-              <Label for="password">Password</Label>
-              <a
-                  href="/forgot-password"
-                  class="ml-auto inline-block text-sm underline"
-              >
-                Forgot your password?
-              </a>
-            </div>
-            <Input id="password" type="password" required />
-          </div>
-          <Button type="submit" class="w-full">
-            Login
-          </Button>
-          <Button variant="outline" class="w-full">
-            Login with Google
-          </Button>
+          <Input id="password" type="password" required />
         </div>
-        <div class="mt-4 text-center text-sm">
-          Don't have an account?
-          <a href="#" class="underline">
-            Sign up
-          </a>
-        </div>
+        <Button type="submit" class="w-full">
+          Login
+        </Button>
       </div>
-    </div>
-    <div class="hidden bg-muted lg:block">
-<!--      <img-->
-<!--          src="/placeholder.svg"-->
-<!--          alt="Image"-->
-<!--          width="1920"-->
-<!--          height="1080"-->
-<!--          class="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"-->
-<!--      >-->
-    </div>
-  </div>
+      <div class="mt-4 text-center text-sm">
+        Don't have an account?
+        <a href="#" class="underline">
+          Sign up
+        </a>
+      </div>
+    </CardContent>
+  </Card>
 </template>
