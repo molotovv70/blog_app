6e1a8bebf46dd6ca54ece6c69a7a8d00495c41ef
0xEnthusiast <sergexa.kulikov2003@gmail.com>
2024-06-26 01:45:15 +0500

    build: install pinia

--- client/src/main.ts
+++ client/src/main.ts
@@ -2,8 +2,10 @@ import { createApp } from 'vue'
 import '@/assets/styles/style.css'
 import App from './App.vue'
 
+import { pinia } from "@/lib/pinia.ts";
 import router from '@/lib/router.ts'
 
 createApp(App)
+    .use(pinia)
     .use(router)
     .mount('#app')
