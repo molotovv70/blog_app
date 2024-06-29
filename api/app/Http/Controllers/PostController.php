02b87ad48b2ab1f055381579b2f9cdeeb24f4516
0xEnthusiast <sergexa.kulikov2003@gmail.com>
2024-06-30 01:46:12 +0500

    feat: add post request validation

--- api/app/Http/Controllers/PostController.php
+++ api/app/Http/Controllers/PostController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Contracts\Repository\PostRepositoryContract;
+use App\Http\Requests\Post\StoreRequest;
+use App\Http\Requests\Post\UpdateRequest;
+use App\Http\Resources\PostResource;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
@@ -20,11 +23,11 @@ class PostController extends Controller
         return response()->json($posts);
     }
 
-    public function store(Request $request)
+    public function store(StoreRequest $request): PostResource
     {
         $data = $request->all();
-        $post = $this->postRepository->create($request->all());
-        return response()->json($post, 201);
+        $post = $this->postRepository->create($data);
+        return PostResource::make($post);
     }
 
     public function show($id)
@@ -33,7 +36,7 @@ class PostController extends Controller
         return response()->json($post);
     }
 
-    public function update(Request $request, $id)
+    public function update(UpdateRequest $request, $id)
     {
         $post = $this->postRepository->update($id, $request->all());
         return response()->json($post);
