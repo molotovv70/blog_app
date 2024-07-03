48808e84e50143f67baf9fb4a1171febd4496279
0xEnthusiast <sergexa.kulikov2003@gmail.com>
2024-07-03 16:58:13 +0500

    build: predis install

--- api/app/Models/Post.php
+++ api/app/Models/Post.php
@@ -6,10 +6,12 @@ use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Laravel\Sanctum\HasApiTokens;
+use Laravel\Scout\Searchable;
 
 class Post extends Model
 {
     use HasFactory;
+    use Searchable;
 
     protected $fillable = [
         'title',
